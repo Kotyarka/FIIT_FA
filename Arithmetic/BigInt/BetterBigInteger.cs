7fe2c38cce847375ea636f6a1ae6b19383bd8c02
Kotyarka <varenik.kotyarka@gmail.com>
2026-04-04 21:54:26 +0300

    constructors done

--- Arithmetic/BigInt/BetterBigInteger.cs
+++ Arithmetic/BigInt/BetterBigInteger.cs
@@ -11,13 +11,12 @@ public sealed class BetterBigInteger : IBigInteger
     private uint[]? _data;
     
     public bool IsNegative => _signBit == 1;
-    
-    /// От массива цифр (little endian)
-    public BetterBigInteger(uint[] digits, bool isNegative = false)
+
+    private void ListConstructor(uint[] digits, bool isNegative)
     {
         if (digits == null)
         {
-            throw new ArgumentNullException();
+            throw new ArgumentNullException(nameof(digits));
         }
 
         int NonZeroIndex = digits.Length - 1;
@@ -32,15 +31,14 @@ public sealed class BetterBigInteger : IBigInteger
             return;
         }
 
-        var normalized =  new uint[NonZeroIndex + 1];
+        var normalized = new uint[NonZeroIndex + 1];
         Array.Copy(digits, normalized, normalized.Length);
 
-        if (normalized.Length == 1) // если слишком маленбкое
+        if (normalized.Length == 1) // если слишком маленькое
         {
             _smallValue = normalized[0];
             _data = null;
             _signBit = (isNegative && normalized[0] != 0) ? 1 : 0;
-
             return;
         }
         else
@@ -50,17 +48,129 @@ public sealed class BetterBigInteger : IBigInteger
             _signBit = isNegative ? 1 : 0;
         }
     }
+
+    /// От массива цифр (little endian)
+    public BetterBigInteger(uint[] digits, bool isNegative = false)
+    {
+        ListConstructor(digits, isNegative);
+    }
     public BetterBigInteger(IEnumerable<uint> digits, bool isNegative = false) 
-        : this(digits?.ToArray() ?? throw new ArgumentNullException(nameof(digits)), isNegative) // переводим ienumerable в массив и вызываем первый конструктор
+        : this(digits?.ToArray() ?? throw new ArgumentNullException(nameof(digits)), isNegative)
     {
     }
     
     public BetterBigInteger(string value, int radix)
     {
-        throw new NotImplementedException();
+        if (value == null || radix == null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        if (radix < 2 || radix > 36)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+         value = value.Trim();
+         
+        if (value.Length == 0)
+        {
+            throw new ArgumentException();
+        }
+
+        bool isNegative = false;
+        if (value[0] == '-')
+        {
+            isNegative = true;
+            value = value.Substring(1);
+        } else if (value[0] == '+')
+        {
+            isNegative = false;
+            value = value.Substring(1);
+        }
+
+        if (value.Length == 0)
+        {
+            throw new ArgumentException();
+        }
+
+        var result = new uint[] { 0 };
+        foreach (char c in value)
+        {
+            int digit = ChatToDigit(c, radix);
+            if (digit < 0 || digit >= radix)
+                throw new FormatException();
+            result = Gorner(result, radix, digit);
+        }
+
+        ListConstructor(result, isNegative);
+    }
+
+    private static int ChatToDigit(char c, int? radix)
+    {
+        if (c >= '0' && c <= '9') 
+            return c - '0';
+        if (c >= 'A' && c <= 'Z')
+            return c - 'A' + 10;
+        if (c >= 'a' && c <= 'z')
+            return c - 'a' + 10;
+        return -1;
     }
     
-    
+    private static uint[] Gorner(uint[] digits, int multiplier, int digit)
+    {
+        digits = MultiplyGorner(digits, multiplier);
+        digits = AddGorner(digits, (uint)digit);
+        return digits;
+    }
+    private static uint[] MultiplyGorner(uint[] digits, int multiplier)
+    {
+        if (multiplier == 0)
+        {
+            return new uint[] { 0 };
+        } else if (multiplier == 1)
+        {
+            return digits;
+        }
+
+        ulong carry = 0;
+        var result = new uint[digits.Length + 1]; // заранее еще один разряд
+
+        for (int i = 0; i < digits.Length; i++)
+        {
+            ulong product = (ulong)digits[i] * (ulong)multiplier + carry;
+            result[i] = (uint)(product & 0xFFFFFFFF); // побитовоо отсекам то, что больше 2**32
+            carry = product >> 32;
+        }
+        if (carry > 0)
+            result[digits.Length] = (uint)carry; // записываем остаток
+        else
+            Array.Resize(ref result, digits.Length + 1);
+
+        return result;       
+    }
+
+   private static uint[] AddGorner(uint[] digits, uint addend)
+    {
+        if (addend == 0)
+        {
+            return digits;
+        }
+
+        ulong carry = addend;
+        var result = new uint[digits.Length];      
+        for (int i = 0; i < digits.Length; i++)
+        {
+            ulong sum = (uint)digits[i] + carry;
+            carry = sum >> 32;
+            result[i] = (uint)(sum & 0xFFFFFFFF);         
+        }
+        if (carry > 0) {
+            Array.Resize(ref result, digits.Length + 1);
+            result[digits.Length] = (uint)carry;
+        }
+        return result;
+    } 
     public ReadOnlySpan<uint> GetDigits()
     {
         return _data ?? [_smallValue];
