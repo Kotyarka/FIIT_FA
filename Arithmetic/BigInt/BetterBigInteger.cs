4fb508cabf9a54c1ff3fecea2dfde351abf554f9
Kotyarka <varenik.kotyarka@gmail.com>
2026-04-06 20:52:38 +0300

    done some operations

--- Arithmetic/BigInt/BetterBigInteger.cs
+++ Arithmetic/BigInt/BetterBigInteger.cs
@@ -237,12 +237,274 @@ public sealed class BetterBigInteger : IBigInteger
     }
     
     
-    public static BetterBigInteger operator +(BetterBigInteger a, BetterBigInteger b) => throw new NotImplementedException();
-    public static BetterBigInteger operator -(BetterBigInteger a, BetterBigInteger b) => throw new NotImplementedException();
-    public static BetterBigInteger operator -(BetterBigInteger a) => throw new NotImplementedException();
-    public static BetterBigInteger operator /(BetterBigInteger a, BetterBigInteger b) => throw new NotImplementedException();
-    public static BetterBigInteger operator %(BetterBigInteger a, BetterBigInteger b) => throw new NotImplementedException();
-    
+    public static BetterBigInteger operator +(BetterBigInteger a, BetterBigInteger b)
+    {
+        if (a == null) throw new ArgumentNullException(nameof(a));
+        if (b == null) throw new ArgumentNullException(nameof(b));
+        
+        // Same sign: add magnitudes
+        if (a.IsNegative == b.IsNegative)
+        {
+            var result = AddMagnitudes(a.GetDigits(), b.GetDigits());
+            return new BetterBigInteger(result, a.IsNegative);
+        }
+        else
+        {
+            int compare = CompareMagnitudes(a.GetDigits(), b.GetDigits());
+            if (compare == 0)
+                return new BetterBigInteger(new uint[] { 0 }, false);
+            
+            bool resultNegative = compare < 0 ? b.IsNegative : a.IsNegative;
+            var result = SubtractMagnitudes(
+                compare >= 0 ? a.GetDigits() : b.GetDigits(),
+                compare >= 0 ? b.GetDigits() : a.GetDigits()
+            );
+            return new BetterBigInteger(result, resultNegative);
+        }
+    }
+
+    public static BetterBigInteger operator -(BetterBigInteger a, BetterBigInteger b)
+    {
+        if (a == null) throw new ArgumentNullException(nameof(a));
+        if (b == null) throw new ArgumentNullException(nameof(b));
+        
+        var negatedB = new BetterBigInteger(b.GetDigits().ToArray(), !b.IsNegative);
+        return a + negatedB;
+    }
+
+    public static BetterBigInteger operator -(BetterBigInteger a)
+    {
+        if (a == null) throw new ArgumentNullException(nameof(a));
+        
+        var digits = a.GetDigits().ToArray();
+        bool isNegative = !a.IsNegative && !IsZero(digits);
+        return new BetterBigInteger(digits, isNegative);
+    }
+
+    public static BetterBigInteger operator /(BetterBigInteger a, BetterBigInteger b)
+    {
+        if (a == null) throw new ArgumentNullException(nameof(a));
+        if (b == null) throw new ArgumentNullException(nameof(b));
+        
+        var bDigits = b.GetDigits();
+        if (IsZero(bDigits))
+            throw new DivideByZeroException();
+        
+        var aDigits = a.GetDigits();
+        if (CompareMagnitudes(aDigits, bDigits) < 0)
+            return new BetterBigInteger(new uint[] { 0 }, false);
+        
+        var result = DivideMagnitudes(aDigits, bDigits, out _);
+        bool resultNegative = a.IsNegative != b.IsNegative;
+        return new BetterBigInteger(result, resultNegative && !IsZero(result));
+    }
+
+    public static BetterBigInteger operator %(BetterBigInteger a, BetterBigInteger b)
+    {
+        if (a == null) throw new ArgumentNullException(nameof(a));
+        if (b == null) throw new ArgumentNullException(nameof(b));
+        
+        var bDigits = b.GetDigits();
+        if (IsZero(bDigits))
+            throw new DivideByZeroException();
+        
+        var aDigits = a.GetDigits();
+        if (CompareMagnitudes(aDigits, bDigits) < 0)
+            return new BetterBigInteger(aDigits.ToArray(), a.IsNegative);
+        
+        DivideMagnitudes(aDigits, bDigits, out uint[] remainder);
+        bool remainderZero = IsZero(remainder);
+        return new BetterBigInteger(remainder, a.IsNegative && !remainderZero);
+    }
+
+    private static uint[] AddMagnitudes(ReadOnlySpan<uint> a, ReadOnlySpan<uint> b)
+    {
+        int maxLen = Math.Max(a.Length, b.Length);
+        var result = new uint[maxLen + 1];
+        ulong carry = 0;
+        
+        for (int i = 0; i < maxLen; i++)
+        {
+            ulong sum = carry;
+            if (i < a.Length) sum += a[i];
+            if (i < b.Length) sum += b[i];
+            
+            result[i] = (uint)(sum & 0xFFFFFFFF);
+            carry = sum >> 32;
+        }
+        
+        if (carry > 0)
+            result[maxLen] = (uint)carry;
+        else
+            Array.Resize(ref result, maxLen);
+        
+        return result;
+    }
+
+    private static uint[] SubtractMagnitudes(ReadOnlySpan<uint> a, ReadOnlySpan<uint> b)
+    {
+        // Assumes a >= b
+        var result = new uint[a.Length];
+        ulong borrow = 0;
+        
+        for (int i = 0; i < a.Length; i++)
+        {
+            ulong sub = (ulong)a[i] - borrow;
+            if (i < b.Length) sub -= b[i];
+            
+            if ((long)sub < 0)
+            {
+                sub += 0x100000000UL;
+                borrow = 1;
+            }
+            else
+            {
+                borrow = 0;
+            }
+            
+            result[i] = (uint)sub;
+        }
+        
+        int nonZeroIndex = result.Length - 1;
+        while (nonZeroIndex > 0 && result[nonZeroIndex] == 0)
+            nonZeroIndex--;
+        
+        if (nonZeroIndex + 1 < result.Length)
+            Array.Resize(ref result, nonZeroIndex + 1);
+        
+        return result;
+    }
+
+    private static int CompareMagnitudes(ReadOnlySpan<uint> a, ReadOnlySpan<uint> b)
+    {
+        if (a.Length != b.Length)
+            return a.Length.CompareTo(b.Length);
+        
+        for (int i = a.Length - 1; i >= 0; i--)
+        {
+            if (a[i] != b[i])
+                return a[i].CompareTo(b[i]);
+        }
+        return 0;
+    }
+
+    private static bool IsZero(ReadOnlySpan<uint> digits)
+    {
+        return digits.Length == 1 && digits[0] == 0;
+    }
+
+    private static uint[] DivideMagnitudes(ReadOnlySpan<uint> dividend, ReadOnlySpan<uint> divisorspan, out uint[] remainder)
+    {
+        var divisor = divisorspan.ToArray();
+        if (CompareMagnitudes(dividend, divisor) < 0)
+        {
+            remainder = dividend.ToArray();
+            return new uint[] { 0 };
+        }
+        
+        int n = divisor.Length;
+        int m = dividend.Length - n;
+        
+        var quotient = new uint[m + 1];
+        remainder = new uint[n];
+        dividend.CopyTo(remainder);
+        
+        uint d = (uint)(0x100000000UL / (divisor[n - 1] + 1));
+        var normalizedDividend = MultiplyGorner(remainder, (int)d);
+        var normalizedDivisor = MultiplyGorner(divisor, (int)d);
+        
+        if (normalizedDividend.Length == n)
+        {
+            var temp = new uint[n + 1];
+            normalizedDividend.CopyTo(temp, 1);
+            normalizedDividend = temp;
+        }
+        
+        for (int j = m; j >= 0; j--)
+        {
+            ulong qhat = ((ulong)normalizedDividend[j + n] << 32) + normalizedDividend[j + n - 1];
+            qhat /= normalizedDivisor[n - 1];
+            
+            if (qhat > 0xFFFFFFFF)
+                qhat = 0xFFFFFFFF;
+            
+            ulong rhat = ((ulong)normalizedDividend[j + n] << 32) + normalizedDividend[j + n - 1] - qhat * normalizedDivisor[n - 1];
+            
+            while (qhat * normalizedDivisor[n - 2] > ((rhat << 32) + normalizedDividend[j + n - 2]))
+            {
+                qhat--;
+                rhat += normalizedDivisor[n - 1];
+                if (rhat > 0xFFFFFFFF)
+                    break;
+            }
+            
+            ulong borrow = 0;
+            for (int i = 0; i < n; i++)
+            {
+                ulong product = qhat * normalizedDivisor[i];
+                ulong diff = normalizedDividend[j + i] - borrow - (product & 0xFFFFFFFF);
+                normalizedDividend[j + i] = (uint)diff;
+                borrow = (product >> 32) + ((diff >> 32) & 1);
+            }
+            
+            ulong finalDiff = normalizedDividend[j + n] - borrow;
+            normalizedDividend[j + n] = (uint)finalDiff;
+            
+            quotient[j] = (uint)qhat;
+            
+            if (finalDiff >> 32 > 0)
+            {
+                quotient[j]--;
+                borrow = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    ulong sum = (ulong)normalizedDividend[j + i] + normalizedDivisor[i] + borrow;
+                    normalizedDividend[j + i] = (uint)sum;
+                    borrow = sum >> 32;
+                }
+                normalizedDividend[j + n] += (uint)borrow;
+            }
+        }
+        
+        int qi = quotient.Length - 1;
+        while (qi > 0 && quotient[qi] == 0)
+            qi--;
+        
+        if (qi + 1 < quotient.Length)
+            Array.Resize(ref quotient, qi + 1);
+        
+        remainder = DivideByDigit(normalizedDividend.Take(n).ToArray(), d);
+        
+        return quotient;
+    }
+
+    private static uint[] DivideByDigit(uint[] digits, uint divisor)
+    {
+        if (divisor == 0)
+            throw new DivideByZeroException();
+        
+        if (digits.Length == 1 && digits[0] == 0)
+            return new uint[] { 0 };
+        
+        var result = new uint[digits.Length];
+        ulong remainder = 0;
+        
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            ulong value = (remainder << 32) + digits[i];
+            result[i] = (uint)(value / divisor);
+            remainder = value % divisor;
+        }
+        
+        int nonZeroIndex = result.Length - 1;
+        while (nonZeroIndex > 0 && result[nonZeroIndex] == 0)
+            nonZeroIndex--;
+        
+        if (nonZeroIndex + 1 < result.Length)
+            Array.Resize(ref result, nonZeroIndex + 1);
+        
+        return result;
+    }
     
     public static BetterBigInteger operator *(BetterBigInteger a, BetterBigInteger b)
        => throw new NotImplementedException("Умножение делегируется стратегии, выбирать необходимо в зависимости от размеров чисел");
